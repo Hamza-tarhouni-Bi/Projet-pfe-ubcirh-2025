4075629ec29e9d286e0f35bb73bd3be01926b018
unknown <tarhounihamza51@gmail.com>
2025-03-10 04:24:25 +0100

    First

---
+++ src/views/admin/Congé.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Congé() {
+  return (
+    <div>Congé</div>
+  )
+}
---
+++ src/views/admin/Personnels.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Personnels() {
+  return (
+   <>
+   
+   <div>
+   <h1>Personnels dashboard</h1>  
+   </div>
+   </>
+  )
+}
